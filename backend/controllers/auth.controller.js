2a06d0a3abe331cf458cabb7d496136302b52fc6
HiT <hiteshsingla209@gmail.com>
2024-11-21 10:13:07 +0530

    my commit

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,13 @@
+
+export async function signup(req, res) {
+    res.send("Sign-Up route");
+}
+
+export async function login(req, res) {
+    res.send("Login route");
+}
+
+export async function logout(req, res) {
+    res.send("Logout route");
+}
+
